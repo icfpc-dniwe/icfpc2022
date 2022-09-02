95521000b7fda779a6df1b79d30d24e802e5cf85
Nikolay Amiantov <ab@fmap.me>
2022-09-02 20:50:17 +0700

    Solve and send solutions

---
+++ solve_problem.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eE
+
+usage() {
+  echo "Usage: $0 problem_id" >&2
+  exit 1
+}
+
+problem_id="$1" && shift || usage
+shift && usage || true
+
+api_token=$(cat api_token)
+img_path="problems/$problem_id.png"
+
+if [ ! -e "$img_path" ]; then
+  curl -O "$img_path" -L "https://cdn.robovinci.xyz/imageframes/$problem_id.png"
+fi
+
+tmpdir=$(mktemp -d)
+cleanup() {
+  rm -rf "$tmpdir"
+}
+trap cleanup INT TERM EXIT
+
+out_path="$tmpdir/solution.txt"
+
+cabal exec solve "problems/$problem_id.png" > "$out_path"
+
+curl -v -H "Authorization: Bearer $api_token" -F "file=@$out_path" "https://robovinci.xyz/api/submissions/$problem_id/create"
