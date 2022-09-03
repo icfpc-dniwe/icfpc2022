4e3c8562ec5f0091ce03d2514c2e0f84c168599e
Andrei Shadrikov <notvuvko@gmail.com>
2022-09-03 14:52:25 +0600

    moves changes

--- src/python/moves.py
+++ src/python/moves.py
@@ -3,7 +3,7 @@ import typing as t
 from .types import Orientation
 
 
-def add_line_cut(block_id: str, orientation: Orientation, offset: int) -> str:
+def add_line_cut_move(block_id: str, orientation: Orientation, offset: int) -> str:
     return f'cut [{block_id}] {orientation} [{offset}]'
 
 
@@ -17,3 +17,87 @@ def add_color_move(block_id: str, color: t.Union[np.ndarray, t.Tuple[int, int, i
 
 def add_merge_move(left_block_id: str, right_block_id: str) -> str:
     return f'merge [{left_block_id}] [{right_block_id}]'
+
+
+def add_swap_move(left_block_id: str, right_block_id: str) -> str:
+    return f'swap [{left_block_id}] [{right_block_id}]'
+
+
+class Move:
+    @staticmethod
+    def cost() -> int:
+        raise NotImplementedError
+
+    def program(self) -> str:
+        raise NotImplementedError
+
+
+class PointCut(Move):
+    def __init__(self, block_id: str, offset_point: t.Union[np.ndarray, t.Tuple[int, int]]):
+        super().__init__()
+        self.block_id = block_id
+        self.offset_point = offset_point
+
+    @staticmethod
+    def cost() -> int:
+        return 10
+
+    def program(self) -> str:
+        return add_point_cut_move(self.block_id, self.offset_point)
+
+
+class LineCut(Move):
+    def __init__(self, block_id: str, orientation: Orientation, offset: int):
+        super().__init__()
+        self.block_id = block_id
+        self.orientation = orientation
+        self.offset = offset
+
+    @staticmethod
+    def cost() -> int:
+        return 7
+
+    def program(self) -> str:
+        return add_line_cut_move(self.block_id, self.orientation, self.offset)
+
+
+class Color(Move):
+    def __init__(self, block_id: str, color: t.Union[np.ndarray, t.Tuple[int, int, int, int]]):
+        super().__init__()
+        self.block_id = block_id
+        self.color = color
+
+    @staticmethod
+    def cost() -> int:
+        return 5
+
+    def program(self) -> str:
+        return add_color_move(self.block_id, self.color)
+
+
+class Swap(Move):
+    def __init__(self, left_block_id: str, right_block_id: str):
+        super().__init__()
+        self.left_block_id = left_block_id
+        self.right_block_id = right_block_id
+
+    @staticmethod
+    def cost() -> int:
+        return 3
+
+    def program(self) -> str:
+        return add_swap_move(self.left_block_id, self.right_block_id)
+
+
+class Merge(Move):
+    def __init__(self, left_block_id: str, right_block_id: str):
+        super().__init__()
+        self.left_block_id = left_block_id
+        self.right_block_id = right_block_id
+
+    @staticmethod
+    def cost() -> int:
+        return 1
+
+    def program(self) -> str:
+        return add_merge_move(self.left_block_id, self.right_block_id)
