34628ecbfeb03260ec142431663e0ca4579b0554
Andrei Shadrikov <notvuvko@gmail.com>
2022-09-03 19:22:34 +0600

    .

--- src/python/solvers/line_print.py
+++ src/python/solvers/line_print.py
@@ -51,8 +51,9 @@ def render_by_line(img: RGBAImage) -> t.Tuple[RGBAImage, t.List[Move]]:
                     cur_prefix = cur_columns.pop()
                 new_canvas[:cv_line+1, cur_x:] = img[cv_line, cur_x]
                 program.append(Color(f'{cur_prefix}', img[cv_line, cur_x]))
-                program.append(LineCut(f'{cur_prefix}', Orientation.X, cur_x))
-                cur_columns += [f'{cur_prefix}.0', f'{cur_prefix}.1']
+                if cur_x + 1 < w:
+                    program.append(LineCut(f'{cur_prefix}', Orientation.X, cur_x + 1))
+                    cur_columns += [f'{cur_prefix}.0', f'{cur_prefix}.1']
         if cur_line + 1 < h:
             # securing just printed line
             if len(cur_columns) > 1:
